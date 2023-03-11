b23570f569a4074643c09f5eba874084a487d368
Octav Popescu <octav@cmu.edu>
2023-03-11 00:51:24 -0500

    Implemented templates & style parameters.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import '$utils/commShellEventHandlers.js';
 window.drag = {};
 window.drop = {};
 
+const searchParams = new URL(window.location).searchParams,
+      templates = searchParams.get('templates'),
+      style = searchParams.get('style');
+if (templates) window.templates = templates;
+if (style) document.documentElement.setAttribute('id', style);
+
 const app = new App({
   target: document.getElementById('app'),
 })
